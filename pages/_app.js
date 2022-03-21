3a8c75da104e414c72a14fb45e224c96d3578286
Theodoros <theogriv@gmail.com>
2022-03-21 16:05:20 +0200

    initial commit
    Signed-off-by: Theodoros <theogriv@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />{" "}
+      <footer>
+        <p>© Pixel Cyclops</p>
+      </footer>
+    </div>
+  );
 }
-
-export default MyApp
+export default MyApp;
